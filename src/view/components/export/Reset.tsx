340ff0f1c0550c59e6a456a28a8b03ae9c56e22a
BlueLdr <blueldr.eg@gmail.com>
2021-07-31 16:31:44 -0400

    Added option to clear tutorials in Reset Storage

--- src/view/components/export/Reset.tsx
+++ src/view/components/export/Reset.tsx
@@ -1,8 +1,10 @@
 import * as Preact from "preact";
 import { reset_all_storage } from "~/common";
+import { useStateIfMounted } from "~/view/utils";
 
 export const ResetStorage: Preact.FunctionComponent<{ onCancel: () => void }> =
   ({ onCancel }) => {
+    const [clear_help, set_clear_help] = useStateIfMounted(false);
     return (
       <Preact.Fragment>
         <div className="modal-body tts-reset">
@@ -16,6 +18,15 @@ export const ResetStorage: Preact.FunctionComponent<{ onCancel: () => void }> =
             sample data. If there's anything you might want to keep, go back and
             export the data before resetting.
           </p>
+          <label className="checkbox">
+            <input
+              type="checkbox"
+              className="checkbox-input"
+              checked={clear_help}
+              onInput={() => set_clear_help(!clear_help)}
+            />
+            <span className="checkbox-label">Reset Tutorials</span>
+          </label>
         </div>
         <div className="modal-footer">
           <button className="btn btn-large" onClick={onCancel}>
@@ -23,7 +34,7 @@ export const ResetStorage: Preact.FunctionComponent<{ onCancel: () => void }> =
           </button>
           <button
             className="btn btn-large btn-negative"
-            onClick={reset_all_storage}
+            onClick={() => reset_all_storage(clear_help)}
           >
             Continue
           </button>
